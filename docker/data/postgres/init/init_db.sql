de4ad506b9d37db16f900a2a5cf3dab69209d414
Altin <kelmendialtin48@gmail.com>
2025-05-10 15:08:55 +0200

    Update init_db.sql

--- docker/data/postgres/init/init_db.sql
+++ docker/data/postgres/init/init_db.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS transaction_service_db;
 CREATE DATABASE transaction_service_db;
 
-\c citibike_db;
+\c transaction_service_db;
 
 --TODO: create Tables and fill some test Users and so on
