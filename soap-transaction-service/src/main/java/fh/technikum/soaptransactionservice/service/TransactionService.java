6b0250fa340b7b63dbb5d83e267d1edab70ff2dc
altink7 <kelmendialtin48@gmail.com>
2025-05-25 10:49:28 +0200

    code refactor

--- soap-transaction-service/src/main/java/fh/technikum/soaptransactionservice/service/TransactionService.java
+++ soap-transaction-service/src/main/java/fh/technikum/soaptransactionservice/service/TransactionService.java
@@ -2,19 +2,16 @@ package fh.technikum.soaptransactionservice.service;
 
 import fh.technikum.soaptransactionservice.model.Transaction;
 import fh.technikum.soaptransactionservice.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.Instant;
 
+@RequiredArgsConstructor
 @Service
 public class TransactionService {
-
     private final TransactionRepository transactionRepository;
-
-    public TransactionService(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
-    }
 
     public void saveTransaction(Integer id, String name, Instant timestamp, BigDecimal amount) {
         transactionRepository.save(new Transaction(id, name, timestamp, amount));
